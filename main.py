17c42c15b37a1c4906e013b6152dd9f73859d8e3
Shaobai Li <shaobaili3@gmail.com>
2020-10-13 11:55:15 +1100

    only check rank match

--- main.py
+++ main.py
@@ -7,11 +7,17 @@ matchIds = match.getMatchs(PPID_KEY)
 #print(matchIds)
 
 winNum = 0
+matchNum = 0
 for matchNum, matchid in enumerate(matchIds):
     details = match.getDetails(matchid)
+    if details['info']['game_type'] != 'Ranked':
+        continue
+    else:
+        matchNum += 1
     ppids = details['metadata']['participants']
     for count, ppid in enumerate(ppids):
         if ppid != PPID_KEY:
+            #print(ppid)
             print(str(matchNum + 1) + ". " + match.getPlayerName(ppid))
             oppentDetails = details['info']['players'][count]
             if oppentDetails["game_outcome"] == 'loss':
@@ -21,6 +27,6 @@ for matchNum, matchid in enumerate(matchIds):
                 outcome = 'Loss'
             print(outcome + "   " + str(oppentDetails["factions"]) + " " + oppentDetails['deck_code'])
 
-print("Win rate: " + str(winNum/len(matchIds) * 100) + "%" )
+print("Win rate: " + str(winNum/matchNum * 100) + "%" )
 
-print(winNum)
+print(str(winNum) + ' out of ' + str(matchNum))
