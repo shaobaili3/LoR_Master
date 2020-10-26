65bc6c212fd1fdadf3dc12a9cf4bef4b10a85f04
Shaobai Li <shaobaili3@gmail.com>
2020-10-26 23:50:57 +1100

    refactor

--- main.py
+++ main.py
@@ -3,40 +3,42 @@ import constant
 from match import getPlayerPUUID
 
 
-id = getPlayerPUUID('storm', '5961')
+puuid = getPlayerPUUID('storm', '5961')
+
+def checkMatchDetails():
+    matchIds = match.getMatchs(puuid)
+    #print(matchIds)
+
+    winNum = 0
+    matchNum = 0
+    for matchid in matchIds:
+        details = match.getDetails(matchid)
+        if details['info']['game_type'] != 'Ranked':
 
 
-matchIds = match.getMatchs(id)
-#print(matchIds)
-
-winNum = 0
-matchNum = 0
-for matchid in matchIds:
-    details = match.getDetails(matchid)
-    if details['info']['game_type'] != 'Ranked':
-
-
-        continue
-    else:
-        matchNum += 1
-    ppids = details['metadata']['participants']
-    outcome = None
-    oppentDetails = None
-    myDetials = None
-    for count, ppid in enumerate(ppids):
-        if ppid != id:
-            #print(ppid)
-            print(str(matchNum) + ". " + match.getPlayerName(ppid))
-            oppentDetails = details['info']['players'][count]
-            if oppentDetails["game_outcome"] == 'loss':
-                winNum += 1
-                outcome = 'Win'
-            else:
-                outcome = 'Loss'
+            continue
         else:
-            myDetials = details['info']['players'][count]
-    print(outcome + "   " + str(myDetials["factions"]) + str(oppentDetails["factions"]) + " " + oppentDetails['deck_code'])
+            matchNum += 1
+        ppids = details['metadata']['participants']
+        outcome = None
+        oppentDetails = None
+        myDetials = None
+        for count, ppid in enumerate(ppids):
+            if ppid != puuid:
+                #print(ppid)
+                print(str(matchNum) + ". " + match.getPlayerName(ppid))
+                oppentDetails = details['info']['players'][count]
+                if oppentDetails["game_outcome"] == 'loss':
+                    winNum += 1
+                    outcome = 'Win'
+                else:
+                    outcome = 'Loss'
+            else:
+                myDetials = details['info']['players'][count]
+        print(outcome + "   " + str(myDetials["factions"]) + str(oppentDetails["factions"]) + " " + oppentDetails['deck_code'])
 
-print("Win rate: " + str(winNum/matchNum * 100) + "%" )
+    print("Win rate: " + str(winNum/matchNum * 100) + "%" )
 
-print(str(winNum) + ' out of ' + str(matchNum))
+    print(str(winNum) + ' out of ' + str(matchNum))
+
+checkMatchDetails()
