c0a809be0a3b741718fe271d9db50b5fe41461f8
Shaobai <shaobaili3@gmail.com>
2020-12-26 04:59:45 +1100

    Update build command

--- makefile.bat
+++ makefile.bat
@@ -1,5 +1,5 @@
-nuitka --mingw64 --standalone --show-progress --show-memory --plugin-enable=pylint-warnings --recurse-all --output-dir=app/debug --windows-icon=Resource/image.ico --nofollow-import-to=pystray,PIL,aiohttp LoRmaster.py
+nuitka --mingw64 --standalone --show-progress --show-memory --plugin-enable=pylint-warnings --recurse-all --output-dir=app/debug --windows-icon=Resource/image.ico --nofollow-import-to=pystray,PIL,aiohttp LoRMasterTracker.py
 
-nuitka --mingw64 --standalone --show-progress --show-memory --plugin-enable=pylint-warnings --recurse-all --output-dir=app --windows-icon=Resource/image.ico --nofollow-import-to=pystray,PIL,aiohttp --windows-disable-console LoRmaster.py
+nuitka --mingw64 --standalone --show-progress --show-memory --plugin-enable=pylint-warnings --recurse-all --output-dir=app --windows-icon=Resource/image.ico --nofollow-import-to=pystray,PIL,aiohttp --windows-disable-console LoRMasterTracker.py
 
 pause
