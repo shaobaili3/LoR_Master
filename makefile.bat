629c3a1f53d36ca8bf13e19df3228af9ddcd1317
Shaobai <shaobaili3@gmail.com>
2021-01-11 23:39:18 +1100

    add makefile

--- makefile.bat
+++ makefile.bat
@@ -6,3 +6,7 @@ pause
 
 nuitka --mingw64 --standalone --show-progress --show-memory --plugin-enable=pylint-warnings --recurse-all --output-dir=app/debug --windows-icon=test.ico --nofollow-import-to=aiohttp --plugin-enable=qt-plugins LoRMasterInspector.py
 nuitka --mingw64 --standalone --show-progress --show-memory --plugin-enable=pylint-warnings --recurse-all --output-dir=app --windows-icon=test.ico --nofollow-import-to=aiohttp --windows-disable-console --plugin-enable=qt-plugins LoRMasterInspector.py
+
+
+
+pyinstaller.exe --onefile --icon=test.ico --noconsole LoRMasterInspector.py
