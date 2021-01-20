34523b0a7736804b7ab1033e088024df4cd15ee3
Shaobai <shaobaili3@gmail.com>
2021-01-21 06:43:58 +1100

    Update process check

--- process.py
+++ process.py
@@ -1,9 +1,21 @@
 import psutil
 
+
+
+
+logFilePath = None
 for proc in psutil.process_iter():
     try:
         if proc.name() == u"LoR.exe":
-            print(proc)
-            print(proc.cmdline())
+            #print(proc)
+            #proc.cmdline()
+            logFilePath = proc.cmdline()[4]
     except psutil.AccessDenied:
         print("Permission error or access denied on process")
+
+if logFilePath:
+    print(logFilePath)
+else:
+    pass
+
+
