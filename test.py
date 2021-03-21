cc3eaea84a86869f8a5c676ea7c28ebc70b56cb4
Shaobai <shaobaili3@gmail.com>
2021-03-21 23:40:26 +1100

    Update version number

--- test.py
+++ test.py
@@ -1,93 +1,28 @@
-from PyQt5 import QtCore
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
-import constants as cs
-import os
-import sys
+import psutil
+import subprocess
 from setting import Setting, Server
-from local import Local
-from riot import Riot
-from network import Network
-from player import Player
-from inspectorWidget import InspectorWidget
-from Threads.serverThread import ServerThread
-from Threads.trackThread import TrackThread
-from leaderboard import getRankStr
-import datetime
-import deck
+import time
+import constants as c 
 
 
+def runElectron():
+    try:
+        isRuning = False
+        for proc in psutil.process_iter():
+            try:
+                if proc.name() == u"LoRMasterTracker.exe":
+                    isRuning = True
+            except psutil.AccessDenied:
+                print("Permission error or access denied on process")
+                return None
+            except IndexError:
+                print('Index error')
+                return None
 
-class Message(QWidget):
-    def __init__(self, title, message, parent=None):
-        QWidget.__init__(self, parent)
-        self.setLayout(QGridLayout())
-        self.titleLabel = QLabel(title, self)
-        self.titleLabel.setStyleSheet(
-            "font-family: 'Roboto', sans-serif; font-size: 14px; font-weight: bold; padding: 0;")
-        self.messageLabel = QLabel(message, self)
-        self.messageLabel.setStyleSheet(
-            "font-family: 'Roboto', sans-serif; font-size: 12px; font-weight: normal; padding: 0;")
-        self.buttonClose = QPushButton(self)
-        self.buttonClose.setIcon(QIcon("res/close1.png"))
-        self.buttonClose.setFixedSize(14, 14)
-        self.layout().addWidget(self.titleLabel, 0, 0)
-        self.layout().addWidget(self.messageLabel, 1, 0)
-        self.layout().addWidget(self.buttonClose, 0, 1, 2, 1)
-
-class Notification(QWidget):
-    signNotifyClose = pyqtSignal(str)
-    def __init__(self, parent = None):
-        time = datetime.datetime.now()
-        currentTime = str(time.hour) + ":" + str(time.minute) + "_"
-        self.LOG_TAG = currentTime + self.__class__.__name__ + ": "
-        super(QWidget, self).__init__(parent)
-
-        self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint)
-        resolution = QDesktopWidget().screenGeometry(-1)
-        screenWidth = resolution.width()
-        screenHeight = resolution.height()
-        print(self.LOG_TAG + "width: " + str(resolution.width()) + " height: " + str(resolution.height()))
-        self.nMessages = 0
-        self.mainLayout = QVBoxLayout(self)
-        self.move(screenWidth, 0)
-
-    def setNotify(self, title, message):
-        m = Message(title, message, self)
-        self.mainLayout.addWidget(m)
-        m.buttonClose.clicked.connect(self.onClicked)
-        self.nMessages += 1
-        self.show()
-
-    def onClicked(self):
-        self.mainLayout.removeWidget(self.sender().parent())
-        self.sender().parent().deleteLater()
-        self.nMessages -= 1
-        self.adjustSize()
-        if self.nMessages == 0:
-            self.close()
-
-class Example(QWidget):
-    counter = 0
-
-    def __init__(self, parent=None):
-        QWidget.__init__(self, parent)
-        self.setLayout(QVBoxLayout())
-        btn = QPushButton("Send Notify", self)
-        self.layout().addWidget(btn)
-
-        self.notification = Notification()
-        btn.clicked.connect(self.notify)
+        if isRuning is False:
+            subprocess.Popen('UI/app/LoRMasterTrackerUI-win32-x64/LoRMasterTrackerUI.exe', shell=False)
+    except Exception as e:
+        print('runElectron error:', e)
 
 
-    def notify(self):
-        self.counter += 1
-        self.notification.setNotify("Title{}".format(self.counter),
-                                    "message{}".format(self.counter))
-
-
-app = QApplication(sys.argv)
-w = Example()
-w.show()
-sys.exit(app.exec_())
+runElectron()
