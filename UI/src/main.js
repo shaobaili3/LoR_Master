be114d5ea96a363ab1f3127864704b34bb902e91
painttist <painttist@gmail.com>
2021-04-04 23:24:13 +0800

    Delete google key

--- UI/src/main.js
+++ UI/src/main.js
@@ -5,9 +5,6 @@ import App from './App.vue'
 import router from './router/'
 import '@/assets/css/global.css'
 
-// import firebase from 'firebase/app'
-// import 'firebase/performance'
-
 const store = createStore({
     state () {
         return {
@@ -24,26 +21,5 @@ const store = createStore({
       }
     }
   })
-
-// if (firebase.apps.length === 0) {
-
-//     console.log("Init Firebase App")
-
-//     var firebaseConfig = {
-//     apiKey: "AIzaSyCxGjwqMuzBJXPWz1ixhpFLpH0Gn-SMIl0",
-//     authDomain: "lor-master-leaderboard.firebaseapp.com",
-//     databaseURL: "https://lor-master-leaderboard-default-rtdb.firebaseio.com",
-//     projectId: "lor-master-leaderboard",
-//     storageBucket: "lor-master-leaderboard.appspot.com",
-//     messagingSenderId: "659164123299",
-//     appId: "1:659164123299:web:88206dcb77fc2d81642f16",
-//     measurementId: "G-Q7ZNPR6Y79"
-//     };
-//     // Initialize Firebase
-//     firebase.initializeApp(firebaseConfig);
-//     const perf = firebase.performance();
-//     console.log(perf)
-//     // firebase.analytics();
-// }
 
 createApp(App).use(router).use(store).mount('#app')
