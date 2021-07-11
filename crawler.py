6b13919c9f098da60c0fc07633da2d987045ef6f
Shaobai Li <shaobaili3@gmail.com>
2021-07-12 03:39:20 +1000

    Update crawler

--- crawler.py
+++ crawler.py
@@ -7,35 +7,12 @@ from Models import utility
 from Models import local
 from Models import leaderboard
 import json
-setting = setting.Setting()
-setting.setServer(Server.NA)
-network = network.Network(setting)
-riot = riot.Riot(network)
-# asia europe americas
-
-leaderboard.updateAll()
-
-board = leaderboard.leaderboards[0]['players']
-
-masterNames = []
-
-masterFullName = {}
-
-puuids = {}
-
-# masterFullName['Sirturmund'] = 'NA1'
-
-localTag = local.Local(setting)
-
 
 def loadJson():
     try:
         with open('data.json', 'r') as fp:
             global masterFullName
             masterFullName = json.load(fp)
-        with open('puuid.json', 'r') as fp:
-            global puuids
-            puuids = json.load(fp)
     except IOError as e:
         print('No cache found', e)
         return
@@ -44,15 +21,11 @@ def loadJson():
 def save():
     with open('data.json', 'w+') as fp:
         json.dump(masterFullName, fp)
-    with open('puuid.json', 'w+') as fp:
-        json.dump(puuids, fp)
 
 
 def getMasterPlayersNames():
     for player in board:
         masterNames.append(player['name'])
-    print(masterNames)
-
 
 def getParticipantsPuuids(name, tag):
     puuid = riot.getPlayerPUUID(name, tag)
@@ -60,27 +33,31 @@ def getParticipantsPuuids(name, tag):
     winNum = 0
     matchNum = 0
     if matchIds is None:
-        print("查询失败")
+        print("MatchIDs empty", name, tag)
         return
+
     for matchid in matchIds:
-        if matchNum == 6:
+        if matchNum == 5:
             break
         details = riot.getDetail(matchid)
 
         if details is None:
             continue
 
+        #To-do add retry here
         if str(details).isdigit():
             continue
 
-        print(details)
+        if details['info']['game_type'] != 'Ranked':
+            continue
+
+        # print(details)
         twoPuuid = details['metadata']['participants']
         for count, puuid in enumerate(twoPuuid):
             name = riot.getPlayerName(puuid)
             print(name)
             masterFullName[name[0]] = name[1]
-            full = [name[0] , name[1]]
-            puuids[puuid] = full
+            full = [name[0], name[1]]
             save()
 
 
@@ -89,13 +66,14 @@ def getTagByName(name):
     if tag is None:
         localTag.updateTagByName(name)
         tag = localTag.opponentTag
+        print(name, tag, 'From YI list')
+    print(name, tag)
     return tag
 
 
 def getFull():
     print(masterNames)
     for name in masterNames:
-        print(name, '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
         tag = getTagByName(name)
         if tag is None:
             continue
@@ -104,6 +82,17 @@ def getFull():
         save()
         print('!!!!!!!!!', len(masterFullName), 'found')
 
+
+setting = setting.Setting()
+setting.setServer(Server.NA)
+network = network.Network(setting)
+riot = riot.Riot(network)
+# asia europe americas
+leaderboard.updateAll()
+board = leaderboard.leaderboards[0]['players']
+masterNames = []
+masterFullName = {}
+localTag = local.Local(setting)
 loadJson()
 getMasterPlayersNames()
 getFull()
