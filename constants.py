982b8f639cb13ce5f7945cf652ec53649b4574fc
Shaobai Li <shaobaili3@gmail.com>
2021-08-16 22:16:18 +1000

    v0.9.5.2

--- constants.py
+++ constants.py
@@ -4,11 +4,11 @@ IP_KEY = 'http://127.0.0.1:'
 # PLAYERS_NA_PATH = 'Resource/NA.dat'
 # PLAYERS_EU_PATH = 'Resource/EU.dat'
 # PLAYERS_ASIA_PATH = 'Resource/ASIA.dat'
-VERSION_NUM = '0.9.5.1 beta'
-VERSION_NUM_INSPECTOR = '0.9.5 beta'
+VERSION_NUM = '0.9.5.2 beta'
+VERSION_NUM_INSPECTOR = '0.9.5.2 beta'
 DISPLAY_TITLE = 'LoR Master Tracker'
 #DISPLAY_TITLE = 'LoR大师'
 MAX_NUM_DETAILS = 20
-MAX_NUM_TRACK = 20
+MAX_NUM_TRACK = 10
 
 DefaultLanguage = 'en-US'
