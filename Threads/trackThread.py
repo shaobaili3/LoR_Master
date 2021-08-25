8c38f596f2a7850a40a5ad3255d3380d5866c471
Shaobai Li <shaobaili3@gmail.com>
2021-08-25 21:20:18 +1000

    update tracker refresh from 1 to 0.5

--- Threads/trackThread.py
+++ Threads/trackThread.py
@@ -18,7 +18,7 @@ class TrackThread(QThread):
     def run(self):
         print('tracker running')
         while (True):
-            time.sleep(1)
+            time.sleep(0.5)
             # print('tracking detecting')
             self.local.updateStatus(self.player.checkOpponent,
                                     self.showMessageTrigger.emit,
