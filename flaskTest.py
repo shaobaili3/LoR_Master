805b642c17c0adccd9675f69ee2e4d4f01bdfbc0
Shaobai Li <shaobaili3@gmail.com>
2021-08-25 11:44:46 +1000

    add server in the request

--- flaskTest.py
+++ flaskTest.py
@@ -1,10 +1,12 @@
 from flask import Flask, jsonify
 import json
+from Models.setting import Server
 from Models.player import Player
 from Models.network import Network
 from Models.riot import Riot
 from Models.local import Local
 from Models.setting import Setting
+from Models.leaderboard import checkRank
 
 
 app = Flask(__name__)
@@ -17,7 +19,7 @@ networkInspect = Network(settingInspect)
 riotInspect = Riot(networkInspect)
 playerInspect = Player(riotInspect)
 localInspect = Local(settingInspect)
-localInspect.updatePlayernames()
+#localInspect.updatePlayernames()
 
 print(localInspect.playernames)
 
@@ -28,10 +30,14 @@ def processMatchDetail(detail):
         print('processMatchDetail error:', e)
         return detail
     playernames = []
+    player_info = []
     for puuid in playerPuuids:
         name, tag = riotInspect.getPlayerName(puuid)
+        rank, lp = checkRank(name, Server.NA.value)
         playernames.append(name + '#' + tag)
+        player_info.append({'name':name, 'tag':tag, 'rank':rank, 'lp':lp})
     detail['playernames'] = playernames
+    detail['player_info'] = player_info
     return detail
 
 @app.route("/code", methods = ['get'])
@@ -39,8 +45,10 @@ def get_code():
     return jsonText
 
 
-@app.route("/name/<string:playername>", methods = ['get'])
-def get_names(playername):
+@app.route("/name/<string:server>/<string:playername>", methods = ['get'])
+def get_names(server, playername):
+    settingInspect.setServer(Server._value2member_map_[server])
+    localInspect.updatePlayernames()
     playerList = set()
     for name in localInspect.playernames:
         if name[0:len(playername)].lower() == playername.lower():
@@ -50,8 +58,9 @@ def get_names(playername):
     print(returnList)
     return returnList
 
-@app.route("/search/<string:name>/<string:tag>", methods = ['get'])
-def search(name, tag):
+@app.route("/search/<string:server>/<string:name>/<string:tag>", methods = ['get'])
+def search(name, tag, server):
+    settingInspect.setServer(Server._value2member_map_[server])
     allMatches = []
     try:
         puuid = riotInspect.getPlayerPUUID(name, tag)
