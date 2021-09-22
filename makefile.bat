a528cc25dd53ce654f63c1ab01fd0778c7cdd08a
Shaobai Li <shaobaili3@gmail.com>
2021-09-22 22:11:00 +1000

    add force copy Resource to package folder

--- makefile.bat
+++ makefile.bat
@@ -1,3 +1,5 @@
 
 @REM pyinstaller.exe --icon=logo.ico --noconsole --distpath=./app LoRMasterTracker.py
 pyinstaller.exe --icon=./Resource/logo.ico --distpath=./UI/backend LMTService.py
+xcopy /S /Q /Y /F  "%CD%"\Resource "%CD%"\UI\backend\LMTService\Resource\
+pause
