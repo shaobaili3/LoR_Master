55cf6b5c6f8a6dc0b50e245d0016189908ba66b5
Yifan Tu <painttist@gmail.com>
2021-09-29 16:36:14 +0800

    Makes custom select prettier

---
+++ UI/src/components/BaseSelect.vue
@@ -0,0 +1,123 @@
+<template>
+	<div class="custom-select" :tabindex="tabindex" @blur="open = false">
+		<div class="selected" :class="{ open: open }" @click="open = !open">
+			{{ selected }}
+		</div>
+		<div class="items" :class="{ selectHide: !open }">
+			<div
+				v-for="(option, i) of options"
+				:key="i"
+				@click="
+					selected = option;
+					open = false;
+					$emit('input', option);
+				"
+			>
+				{{ option }}
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		options: {
+			type: Array,
+			required: true,
+		},
+		default: {
+			type: String,
+			required: false,
+			default: null,
+		},
+		tabindex: {
+			type: Number,
+			required: false,
+			default: 0,
+		},
+	},
+	data() {
+		return {
+			selected: this.default
+				? this.default
+				: this.options.length > 0
+				? this.options[0]
+				: null,
+			open: false,
+		};
+	},
+	mounted() {
+		this.$emit("input", this.selected);
+	},
+};
+</script>
+
+<style scoped>
+
+.custom-select {
+	position: relative;
+	width: 160px;
+	text-align: left;
+	outline: none;
+	height: 36px;
+	line-height: 36px;
+    font-size: 0.9em;
+}
+
+.custom-select .selected {
+	background-color: var(--col-dark-grey);
+	border-radius: 6px;
+	/* border: 1px solid #666666; */
+	color: #fff;
+	padding-left: 1em;
+	cursor: pointer;
+	user-select: none;
+}
+
+.custom-select .selected.open {
+	/* border: 1px solid #ad8225; */
+	border-radius: 6px 6px 0px 0px;
+}
+
+.custom-select .selected:after {
+	position: absolute;
+	content: "";
+	top: 18px;
+	right: 1em;
+	width: 0;
+	height: 0;
+	border: 5px solid transparent;
+	border-color: #fff transparent transparent transparent;
+}
+
+.custom-select .items {
+	color: #fff;
+	border-radius: 0px 0px 6px 6px;
+	overflow: hidden;
+	/* border-right: 1px solid #ad8225; */
+	/* border-left: 1px solid #ad8225; */
+	/* border-bottom: 1px solid #ad8225; */
+	position: absolute;
+	background-color: var(--col-dark-grey);
+	left: 0;
+	right: 0;
+	z-index: 1;
+}
+
+.custom-select .items div {
+	color: #fff;
+	padding-left: 1em;
+	cursor: pointer;
+	user-select: none;
+}
+
+.custom-select .items div:hover {
+	background-color: var(--col-light-grey);
+}
+
+.selectHide {
+	display: none;
+}
+</style>
+
--- UI/src/components/LocaleChanger.vue
+++ UI/src/components/LocaleChanger.vue
@@ -1,33 +1,47 @@
 <template>
-    <div class="locale-changer">
-        <div class="title">
-            {{$t('str.languages')}}
-        </div>
-        <div class="select-container">
-            <select v-model="$i18n.locale">
-                <option v-for="locale in $i18n.availableLocales" :key="`locale-${locale}`" :value="locale">{{ locale }}</option>
-            </select>
-        </div>
+  <div class="locale-changer">
+    <div class="title">
+      {{$t('str.languages')}}
     </div>
+    <div class="select-container">
+      <!-- <select v-model="$i18n.locale">
+        <option v-for="locale in $i18n.availableLocales" :key="`locale-${locale}`" :value="locale">{{ locale }}</option>
+      </select> -->
+      <base-select :options="$i18n.availableLocales" :default="$i18n.locale" @input="changeLocale($event)"></base-select>
+    </div>
+  </div>
 </template>
 
-<style scoped>
-    .locale-changer {
-        display: flex;
-        justify-content: space-between;
-        width: 100%;
+<script>
+import BaseSelect from './BaseSelect.vue'
+export default {
+  components: { BaseSelect },
+  methods: {
+    changeLocale(newLocale) {
+      this.$i18n.locale = newLocale
     }
+  }
+}
+</script>
 
-    select {
-        font-size: 0.9em;
-        outline: none;
-        background: var(--col-dark-grey);
-        color: white;
-        padding: 8px 15px;
-        border-radius: 40px;
-        border: 0px;
-        appearance: none;
-        cursor: pointer;
-    }
+<style scoped>
+  .locale-changer {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+  }
+
+  select {
+    font-size: 0.9em;
+    outline: none;
+    background: var(--col-dark-grey);
+    color: white;
+    padding: 8px 15px;
+    border-radius: 40px;
+    border: 0px;
+    appearance: none;
+    cursor: pointer;
+  }
 
 </style>
