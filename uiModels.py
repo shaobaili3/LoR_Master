5f1e04ae6c0d831ced8ac0d9c784f38dda432027
Shaobai Li <Shaobaili3@gmail.com>
2021-10-02 00:42:51 +1000

    Fix no deckCode in history
    update changelog

--- uiModels.py
+++ uiModels.py
@@ -1,7 +1,8 @@
 class DeckSummary:
-    def __init__(self, matches: int , winNum: int, time: str, startTime: str):
+    def __init__(self, matches: int, winNum: int, time: str, startTime: str, deckCode: str):
         self.matches = matches
         self.winNum = winNum
         self.time = time
         self.history = ''
         self.startTime = startTime
+        self.deckCode = deckCode
