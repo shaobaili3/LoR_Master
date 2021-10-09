5e1e764fea47bf22e876f162edf31cd426e08dc0
painttist <painttist@gmail.com>
2021-10-10 03:49:12 +0800

    Adds a template for building main and deck windows

--- UI/src/pages/home/main.js
+++ UI/src/pages/home/main.js
@@ -1,47 +1,5 @@
-import { createApp } from 'vue'
-import { createI18n } from 'vue-i18n'
-
 import App from './Home.vue'
-import '@/assets/css/global.css'
 
-import { createStore, mapState, mapMutations } from 'vuex'
+import template from '../template'
 
-const store = createStore({
-  state () {
-    return {
-      locale: 'en_us'
-    }
-  },
-  mutations: {
-    changeLocale (state, newLocale) {
-        state.locale = newLocale
-    }
-  }
-})
-
-const messages = require('@/assets/data/messages.js')
-
-const i18n = createI18n({
-  locale: 'English', // set locale
-  fallbackLocale: 'English', // set fallback locale
-  messages,
-})
-
-const app = createApp(App)
-
-app.use(i18n).use(store)
-
-app.mixin({
-  computed: {
-    ...mapState([
-      'locale'
-    ])
-  },
-  // methods: {
-  //   ...mapMutations([
-  //     'changeLocale'
-  //   ]),
-  // }
-})
-
-app.mount('#app')
+template(App)
---
+++ UI/src/pages/template.js
@@ -0,0 +1,48 @@
+import { createApp } from 'vue'
+import { createI18n } from 'vue-i18n'
+import '@/assets/css/global.css'
+
+import { createStore, mapState, mapMutations } from 'vuex'
+
+export default (App) => {
+
+const store = createStore({
+    state () {
+        return {
+            locale: 'en_us'
+        }
+    },
+    mutations: {
+        changeLocale (state, newLocale) {
+                state.locale = newLocale
+        }
+    }
+})
+
+const messages = require('@/assets/data/messages.js')
+
+const i18n = createI18n({
+    locale: 'English', // set locale
+    fallbackLocale: 'English', // set fallback locale
+    messages,
+})
+
+const app = createApp(App)
+
+app.use(i18n).use(store)
+
+app.mixin({
+    computed: {
+        ...mapState([
+            'locale'
+        ])
+    },
+    // methods: {
+    //   ...mapMutations([
+    //     'changeLocale'
+    //   ]),
+    // }
+})
+
+app.mount('#app')
+}
