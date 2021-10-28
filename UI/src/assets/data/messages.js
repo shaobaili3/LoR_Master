1c51b9b654bb59b1f6ad8844bcb217b631d9452d
Shaobai Li <Shaobaili3@gmail.com>
2021-10-28 16:30:28 +1100

    add jp localization by 月影

--- UI/src/assets/data/messages.js
+++ UI/src/assets/data/messages.js
@@ -1,398 +1,499 @@
 module.exports = {
   English: {
-    appName: 'LoR Master Tracker',
+    appName: "LoR Master Tracker",
     str: {
-      settings: 'Settings',
-      loading: 'Loading...',
-      languages: 'Main UI Languages',
-      invalidDeck: 'Invalid Deck Code',
-      copy: 'Copy',
-      copied: 'Copied!',
-      rounds: 'Rounds',
+      settings: "Settings",
+      loading: "Loading...",
+      languages: "Main UI Languages",
+      invalidDeck: "Invalid Deck Code",
+      copy: "Copy",
+      copied: "Copied!",
+      rounds: "Rounds",
       error: {
-        unkown: 'Something went wrong',
-        playerNotFound: 'Player not found',
-        playerNoHistory: 'No history found',
-        localApiError: 'Please enable 3rd party API'
+        unkown: "Something went wrong",
+        playerNotFound: "Player not found",
+        playerNoHistory: "No history found",
+        localApiError: "Please enable 3rd party API",
       },
       regions: {
-        americas: 'Americas',
-        europe: 'Europe',
-        asia: 'Asia',
-        sea: 'SEA',
+        americas: "Americas",
+        europe: "Europe",
+        asia: "Asia",
+        sea: "SEA",
       },
       times: {
-        sec: '{t} sec. ago',
-        min: '{t} min. ago',
-        hour: '{t} hour ago',
-        hours: '{t} hours ago',
-        day: '{t} day ago',
-        days: '{t} days ago',
+        sec: "{t} sec. ago",
+        min: "{t} min. ago",
+        hour: "{t} hour ago",
+        hours: "{t} hours ago",
+        day: "{t} day ago",
+        days: "{t} days ago",
       },
     },
     loading: {
-      readyToRock: 'Ready to rock 🤘',
-      history: 'Loading History...',
-      nohistory: 'History unavailable',
+      readyToRock: "Ready to rock 🤘",
+      history: "Loading History...",
+      nohistory: "History unavailable",
     },
     tracker: {
       tabs: {
-        oppoPlayed: 'Opponent Played',
-        myPlayed: 'My Played',
+        oppoPlayed: "Opponent Played",
+        myPlayed: "My Played",
       },
-      cardsInHand: 'Cards in Hand: {num}'
+      cardsInHand: "Cards in Hand: {num}",
     },
     leaderboard: {
-      rank: 'Rank',
-      name: 'Name',
-      points: 'Points',
+      rank: "Rank",
+      name: "Name",
+      points: "Points",
     },
     matches: {
-      usage: '{num}% Usage',
-      game: '{num} game',
-      games: '{num} games',
+      usage: "{num}% Usage",
+      game: "{num} game",
+      games: "{num} games",
       badges: {
-        Ranked: 'Ranked',
-        Bo3Challenge: 'Bo3 Challenge',
-        StandardGauntlet: 'Gauntlet',
-        Normal: 'Normal',
-        LastCallQualifierGauntlet: 'Last Chance Gauntlet',
-        SeasonalTournament: 'Seasonal',
-        AI: 'AI'
-      }
+        Ranked: "Ranked",
+        Bo3Challenge: "Bo3 Challenge",
+        StandardGauntlet: "Gauntlet",
+        Normal: "Normal",
+        LastCallQualifierGauntlet: "Last Chance Gauntlet",
+        SeasonalTournament: "Seasonal",
+        AI: "AI",
+      },
     },
     dash: {
-      winRate: 'WIN',
-      winloss: '{win}W {loss}L',
+      winRate: "WIN",
+      winloss: "{win}W {loss}L",
     },
     search: {
       leaderboard: {
-        base: 'Search',
-        numPlayer: 'Search {num} players'
+        base: "Search",
+        numPlayer: "Search {num} players",
       },
       player: {
-        placeholder: 'eg.Storm#5961'
+        placeholder: "eg.Storm#5961",
       },
-      prompt: '⬆ Enter name and tag to search',
+      prompt: "⬆ Enter name and tag to search",
     },
     tooltips: {
-      lorlogin: 'Please login LoR',
-      incompleteDeck: 'Incomplete Deck',
+      lorlogin: "Please login LoR",
+      incompleteDeck: "Incomplete Deck",
     },
     contact: {
       messageBox: {
-        placeholder: 'Any suggestions?',
-        messageSent: 'Message Sent',
-        confirm: 'Confirm?',
-        send: 'Send',
+        placeholder: "Any suggestions?",
+        messageSent: "Message Sent",
+        confirm: "Confirm?",
+        send: "Send",
       },
-      contactUs: 'Contact Us',
-      broughtWithLove: 'Brought to you with ❤️ by'
+      contactUs: "Contact Us",
+      broughtWithLove: "Brought to you with ❤️ by",
     },
     settings: {
       options: {
-        autoLaunch: 'Auto launch on startup:'
+        autoLaunch: "Auto launch on startup:",
       },
-      enabled: 'Enabled',
-      enable: 'Enable',
-      disabled: 'Disabled',
-      disable: 'Disable'
-    }
+      enabled: "Enabled",
+      enable: "Enable",
+      disabled: "Disabled",
+      disable: "Disable",
+    },
   },
   繁體中文: {
-    appName: 'LoR Master Tracker 魔盒',
+    appName: "LoR Master Tracker 魔盒",
     str: {
-      settings: '設置',
-      loading: '加載中...',
-      languages: '主界面語言',
-      invalidDeck: '牌組代碼錯誤',
-      copy: '複製',
-      copied: '複製成功！',
-      rounds: '回合',
+      settings: "設置",
+      loading: "加載中...",
+      languages: "主界面語言",
+      invalidDeck: "牌組代碼錯誤",
+      copy: "複製",
+      copied: "複製成功！",
+      rounds: "回合",
       error: {
-        unkown: '哪裡出了一些問題',
-        playerNotFound: '未找到玩家',
-        playerNoHistory: '未找到歷史記錄',
-        localApiError: '請打開第三方端口'
+        unkown: "哪裡出了一些問題",
+        playerNotFound: "未找到玩家",
+        playerNoHistory: "未找到歷史記錄",
+        localApiError: "請打開第三方端口",
       },
       regions: {
-        americas: '美洲',
-        europe: '歐洲',
-        asia: '亞洲',
-        sea: '東南亞',
+        americas: "美洲",
+        europe: "歐洲",
+        asia: "亞洲",
+        sea: "東南亞",
       },
       times: {
-        sec: '{t} 秒前',
-        min: '{t} 分鐘前',
-        hour: '{t} 小時前',
-        hours: '{t} 小時前',
-        day: '{t} 天前',
-        days: '{t} 天前',
+        sec: "{t} 秒前",
+        min: "{t} 分鐘前",
+        hour: "{t} 小時前",
+        hours: "{t} 小時前",
+        day: "{t} 天前",
+        days: "{t} 天前",
       },
     },
     loading: {
-      readyToRock: '準備就緒 🤘',
-      history: '努力加載歷史中...',
-      nohistory: '沒有找到歷史數據',
+      readyToRock: "準備就緒 🤘",
+      history: "努力加載歷史中...",
+      nohistory: "沒有找到歷史數據",
     },
     tracker: {
       tabs: {
-        oppoPlayed: '對手已打出',
-        myPlayed: '我方已打出',
+        oppoPlayed: "對手已打出",
+        myPlayed: "我方已打出",
       },
-      cardsInHand: '手牌數: {num}'
+      cardsInHand: "手牌數: {num}",
     },
     leaderboard: {
-      rank: '排名',
-      name: '玩家',
-      points: '分數',
+      rank: "排名",
+      name: "玩家",
+      points: "分數",
     },
     matches: {
-      usage: '{num}% 使用率',
-      game: '{num} 場',
-      games: '{num} 場',
+      usage: "{num}% 使用率",
+      game: "{num} 場",
+      games: "{num} 場",
       badges: {
-        Ranked: '排位',
-        Bo3Challenge: '三局兩勝',
-        StandardGauntlet: '武鬥場',
-        Normal: '普通',
-        LastCallQualifierGauntlet: '終極武鬥場',
-        SeasonalTournament: '季賽'
-      }
+        Ranked: "排位",
+        Bo3Challenge: "三局兩勝",
+        StandardGauntlet: "武鬥場",
+        Normal: "普通",
+        LastCallQualifierGauntlet: "終極武鬥場",
+        SeasonalTournament: "季賽",
+      },
     },
     dash: {
-      winRate: '勝率',
-      winloss: '{win}勝 {loss}負',
+      winRate: "勝率",
+      winloss: "{win}勝 {loss}負",
     },
     search: {
       leaderboard: {
-        base: '搜索',
-        numPlayer: '搜索 {num} 名玩家'
+        base: "搜索",
+        numPlayer: "搜索 {num} 名玩家",
       },
       player: {
-        placeholder: '例：Storm#5961'
+        placeholder: "例：Storm#5961",
       },
-      prompt: '⬆ 請輸入名字與後綴',
+      prompt: "⬆ 請輸入名字與後綴",
     },
     tooltips: {
-      lorlogin: 'LoR 未登錄 / 無記錄'
+      lorlogin: "LoR 未登錄 / 無記錄",
     },
     contact: {
       messageBox: {
-        placeholder: '來一條建議？',
-        messageSent: '已發送',
-        confirm: '確定？',
-        send: '發送',
+        placeholder: "來一條建議？",
+        messageSent: "已發送",
+        confirm: "確定？",
+        send: "發送",
       },
-      contactUs: '聯繫我們',
-      broughtWithLove: '為你傾❤️打造:'
+      contactUs: "聯繫我們",
+      broughtWithLove: "為你傾❤️打造:",
     },
     settings: {
       options: {
-        autoLaunch: '開機自動啟動:'
+        autoLaunch: "開機自動啟動:",
       },
-      enabled: '開啟',
-      enable: '開啟',
-      disabled: '關閉',
-      disable: '關閉'
-    }
+      enabled: "開啟",
+      enable: "開啟",
+      disabled: "關閉",
+      disable: "關閉",
+    },
   },
   简体中文: {
-    appName: 'LoR Master Tracker 魔盒',
+    appName: "LoR Master Tracker 魔盒",
     str: {
-      settings: '设置',
-      loading: '加载中...',
-      languages: '主界面语言',
-      invalidDeck: '牌组代码错误',
-      copy: '复制',
-      copied: '复制成功！',
-      rounds: '回合',
+      settings: "设置",
+      loading: "加载中...",
+      languages: "主界面语言",
+      invalidDeck: "牌组代码错误",
+      copy: "复制",
+      copied: "复制成功！",
+      rounds: "回合",
       error: {
-        unkown: '哪里出了一些问题',
-        playerNotFound: '未找到玩家',
-        playerNoHistory: '未找到历史记录',
-        localApiError: '请打开第三方端口'
+        unkown: "哪里出了一些问题",
+        playerNotFound: "未找到玩家",
+        playerNoHistory: "未找到历史记录",
+        localApiError: "请打开第三方端口",
       },
       regions: {
-        americas: '美洲',
-        europe: '欧洲',
-        asia: '亚洲',
-        sea: '东南亚',
+        americas: "美洲",
+        europe: "欧洲",
+        asia: "亚洲",
+        sea: "东南亚",
       },
       times: {
-        sec: '{t} 秒前',
-        min: '{t} 分钟前',
-        hour: '{t} 小时前',
-        hours: '{t} 小时前',
-        day: '{t} 天前',
-        days: '{t} 天前',
+        sec: "{t} 秒前",
+        min: "{t} 分钟前",
+        hour: "{t} 小时前",
+        hours: "{t} 小时前",
+        day: "{t} 天前",
+        days: "{t} 天前",
       },
     },
     loading: {
-      readyToRock: '准备就绪 🤘',
-      history: '努力加载历史中...',
-      nohistory: '没有找到历史数据',
+      readyToRock: "准备就绪 🤘",
+      history: "努力加载历史中...",
+      nohistory: "没有找到历史数据",
     },
     tracker: {
       tabs: {
-        oppoPlayed: '对手已打出',
-        myPlayed: '我方已打出',
+        oppoPlayed: "对手已打出",
+        myPlayed: "我方已打出",
       },
-      cardsInHand: '手牌数: {num}'
+      cardsInHand: "手牌数: {num}",
     },
     leaderboard: {
-      rank: '排名',
-      name: '玩家',
-      points: '分数',
+      rank: "排名",
+      name: "玩家",
+      points: "分数",
     },
     matches: {
-      usage: '{num}% 使用率',
-      game: '{num} 场',
-      games: '{num} 场',
+      usage: "{num}% 使用率",
+      game: "{num} 场",
+      games: "{num} 场",
       badges: {
-        Ranked: '排位',
-        Bo3Challenge: '三局两胜',
-        StandardGauntlet: '武斗场',
-        Normal: '普通',
-        LastCallQualifierGauntlet: '终极武斗场',
-        SeasonalTournament: '季赛'
-      }
+        Ranked: "排位",
+        Bo3Challenge: "三局两胜",
+        StandardGauntlet: "武斗场",
+        Normal: "普通",
+        LastCallQualifierGauntlet: "终极武斗场",
+        SeasonalTournament: "季赛",
+      },
     },
     dash: {
-      winRate: '胜率',
-      winloss: '{win}胜 {loss}负',
+      winRate: "胜率",
+      winloss: "{win}胜 {loss}负",
     },
     search: {
       leaderboard: {
-        base: '搜索',
-        numPlayer: '搜索 {num} 名玩家'
+        base: "搜索",
+        numPlayer: "搜索 {num} 名玩家",
       },
       player: {
-        placeholder: '例：Storm#5961'
+        placeholder: "例：Storm#5961",
       },
-      prompt: '⬆ 请输入名字与后缀',
+      prompt: "⬆ 请输入名字与后缀",
     },
     tooltips: {
-      lorlogin: 'LoR 未登录'
+      lorlogin: "LoR 未登录",
     },
     contact: {
       messageBox: {
-        placeholder: '来一条建议？',
-        messageSent: '已发送',
-        confirm: '确定？',
-        send: '发送',
+        placeholder: "来一条建议？",
+        messageSent: "已发送",
+        confirm: "确定？",
+        send: "发送",
       },
-      contactUs: '联系我们',
-      broughtWithLove: '为你倾❤️打造:'
+      contactUs: "联系我们",
+      broughtWithLove: "为你倾❤️打造:",
     },
     settings: {
       options: {
-        autoLaunch: '开机自动启动:'
+        autoLaunch: "开机自动启动:",
       },
-      enabled: '开启',
-      enable: '开启',
-      disabled: '关闭',
-      disable: '关闭'
-    }
+      enabled: "开启",
+      enable: "开启",
+      disabled: "关闭",
+      disable: "关闭",
+    },
   },
-  'Português (Brasil)': {
-    appName: 'LoR Master Tracker',
+  "Português (Brasil)": {
+    appName: "LoR Master Tracker",
     str: {
-      settings: 'Configurações',
-      loading: 'Carregando...',
-      languages: 'Idioma da interface principal',
-      invalidDeck: 'Código de deck inválido',
-      copy: 'Copiar',
-      copied: 'Copiado!',
-      rounds: 'rodadas',
+      settings: "Configurações",
+      loading: "Carregando...",
+      languages: "Idioma da interface principal",
+      invalidDeck: "Código de deck inválido",
+      copy: "Copiar",
+      copied: "Copiado!",
+      rounds: "rodadas",
       error: {
-        unkown: 'Algo deu errado',
-        playerNotFound: 'Jogador não encontrado',
-        playerNoHistory: 'Nenhum histórico encontrado',
-        localApiError: 'Por favor, habilite a opção ferramentas de terceiros no cliente do jogo'
+        unkown: "Algo deu errado",
+        playerNotFound: "Jogador não encontrado",
+        playerNoHistory: "Nenhum histórico encontrado",
+        localApiError:
+          "Por favor, habilite a opção ferramentas de terceiros no cliente do jogo",
       },
       regions: {
-        americas: 'Américas',
-        europe: 'Europa',
-        asia: 'Ásia',
-        sea: 'Sudeste asiático',
+        americas: "Américas",
+        europe: "Europa",
+        asia: "Ásia",
+        sea: "Sudeste asiático",
       },
       times: {
-        sec: '{t} s atrás',
-        min: '{t} min atrás',
-        hour: '{t} hora atrás',
-        hours: '{t} horas atrás',
-        day: '{t} dia atrás',
-        days: '{t} dias atrás',
+        sec: "{t} s atrás",
+        min: "{t} min atrás",
+        hour: "{t} hora atrás",
+        hours: "{t} horas atrás",
+        day: "{t} dia atrás",
+        days: "{t} dias atrás",
       },
     },
     loading: {
-      readyToRock: 'Pronto para arrasar 🤘',
-      history: 'Carregando histórico...',
-      nohistory: 'Histórico indisponível',
+      readyToRock: "Pronto para arrasar 🤘",
+      history: "Carregando histórico...",
+      nohistory: "Histórico indisponível",
     },
     tracker: {
       tabs: {
-        oppoPlayed: 'Oponente jogou',
-        myPlayed: 'Eu joguei',
+        oppoPlayed: "Oponente jogou",
+        myPlayed: "Eu joguei",
       },
-      cardsInHand: 'Cartas na mão: {num}'
+      cardsInHand: "Cartas na mão: {num}",
     },
     leaderboard: {
-      rank: 'Posição',
-      name: 'Nome',
-      points: 'Pontos',
+      rank: "Posição",
+      name: "Nome",
+      points: "Pontos",
     },
     matches: {
-      usage: '{num}% de uso',
-      game: '{num} jogo',
-      games: '{num} jogos',
+      usage: "{num}% de uso",
+      game: "{num} jogo",
+      games: "{num} jogos",
       badges: {
-        Ranked: 'Ranqueada',
-        Bo3Challenge: 'Confronto melhor de três',
-        StandardGauntlet: 'Contenda',
-        Normal: 'Normal',
-        LastCallQualifierGauntlet: 'Contenda final',
-        SeasonalTournament: 'Torneio sazonal',
-        AI: 'IA'
-      }
+        Ranked: "Ranqueada",
+        Bo3Challenge: "Confronto melhor de três",
+        StandardGauntlet: "Contenda",
+        Normal: "Normal",
+        LastCallQualifierGauntlet: "Contenda final",
+        SeasonalTournament: "Torneio sazonal",
+        AI: "IA",
+      },
     },
     dash: {
-      winRate: 'de vitória',
-      winloss: '{win}V {loss}D',
+      winRate: "de vitória",
+      winloss: "{win}V {loss}D",
     },
     search: {
       leaderboard: {
-        base: 'Procurar',
-        numPlayer: 'Procure entre {num} jogadores'
+        base: "Procurar",
+        numPlayer: "Procure entre {num} jogadores",
       },
       player: {
-        placeholder: 'Exemplo: Storm#5961'
+        placeholder: "Exemplo: Storm#5961",
       },
-      prompt: '⬆ Enter name and tag to search',
+      prompt: "⬆ Enter name and tag to search",
     },
     tooltips: {
-      lorlogin: 'Por favor, faça login no LoR',
-      incompleteDeck: 'Deck incompleto',
+      lorlogin: "Por favor, faça login no LoR",
+      incompleteDeck: "Deck incompleto",
     },
     contact: {
       messageBox: {
-        placeholder: 'Sugestões?',
-        messageSent: 'Mensagem enviada',
-        confirm: 'Confirmar?',
-        send: 'Enviar',
+        placeholder: "Sugestões?",
+        messageSent: "Mensagem enviada",
+        confirm: "Confirmar?",
+        send: "Enviar",
       },
-      contactUs: 'Fale conosco!',
-      broughtWithLove: 'Feito para você com ❤️ por'
+      contactUs: "Fale conosco!",
+      broughtWithLove: "Feito para você com ❤️ por",
     },
     settings: {
       options: {
-        autoLaunch: 'Abrir automaticante ao iniciar o sistema:'
+        autoLaunch: "Abrir automaticante ao iniciar o sistema:",
       },
-      enabled: 'Ativado',
-      enable: 'Ativar',
-      disabled: 'Desativado',
-      disable: 'Desativar'
-    }
-  }
-}
+      enabled: "Ativado",
+      enable: "Ativar",
+      disabled: "Desativado",
+      disable: "Desativar",
+    },
+  },
+
+  日本語: {
+    appName: "LoR Master Tracker",
+    str: {
+      settings: "設定",
+      loading: "読み込み中",
+      languages: "言語",
+      invalidDeck: "無効なデッキコード",
+      copy: "コピー",
+      copied: "コピーしました！",
+      rounds: "ラウンド",
+      error: {
+        unkown: "予期せぬエラー",
+        playerNotFound: "プレイヤーが見つかりません",
+        playerNoHistory: "対戦履歴が見つかりません",
+        localApiError:
+          "本ソフトを使うには、ゲーム設定の「サードパーティーエンドポイントを有効にする」をチェックしてください",
+      },
+      regions: {
+        americas: "アメリカサーバー",
+        europe: "ヨーロッパサーバー",
+        asia: "アジアサーバー",
+        sea: "東南アジアサーバー",
+      },
+      times: {
+        sec: "{t} 秒前",
+        min: "{t} 分間前",
+        hour: "{t} 時間前",
+        hours: "{t} 時間前",
+        day: "{t} 日前",
+        days: "{t} 日前",
+      },
+    },
+    loading: {
+      readyToRock: "準備万端 🤘",
+      history: "対戦履歴を読み込み中...",
+      nohistory: "対戦履歴が見つかりません",
+    },
+    tracker: {
+      tabs: {
+        oppoPlayed: "相手がプレイしたカード",
+        myPlayed: "自分がプレイしたカード",
+      },
+      cardsInHand: "手札枚数: {num}",
+    },
+    leaderboard: {
+      rank: "ランキング",
+      name: "プレイヤー名",
+      points: "ポイント",
+    },
+    matches: {
+      usage: "{num}% 使用率",
+      game: "{num} 回",
+      games: "{num} 回",
+      badges: {
+        Ranked: "ランク戦",
+        Bo3Challenge: "フレンドとのBO3対戦",
+        StandardGauntlet: "スタンダードガントレット",
+        Normal: "ノーマル戦",
+        LastCallQualifierGauntlet: "ラストチャンスガントレット",
+        SeasonalTournament: "シーズントーナメント",
+      },
+    },
+    dash: {
+      winRate: "勝率",
+      winloss: "{win}勝ち {loss}負け",
+    },
+    search: {
+      leaderboard: {
+        base: "検索",
+        numPlayer: " {num} 人のプレイヤーから検索",
+      },
+      player: {
+        placeholder: "例えば：Storm#5961",
+      },
+      prompt: "⬆ プレイヤー名とハッシュタグを入力してください",
+    },
+    tooltips: {
+      lorlogin: "LoRがログインしていません",
+    },
+    contact: {
+      messageBox: {
+        placeholder: "アドバイスを送ってくれない？",
+        messageSent: "送信しました",
+        confirm: "確認します？",
+        send: "送信",
+      },
+      contactUs: "開発者を連絡",
+      broughtWithLove: "あなたのために❤️を注いで作った:",
+    },
+    settings: {
+      options: {
+        autoLaunch: "パソコン起動時に本ソフトを自動起動する:",
+      },
+      enabled: "起動する",
+      enable: "起動する",
+      disabled: "起動しない",
+      disable: "起動しない",
+    },
+  },
+};
