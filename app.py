00cfb07ff60101167bd168fb6dc267b28fbe4f62
Shaobai Li <Shaobaili3@gmail.com>
2021-11-03 10:36:10 +1100

    adjust refresh time

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ class FlaskApp(Flask):
         def run_work():
             while True:
                 leaderboardModel.updateAll()
-                time.sleep(2000)
+                time.sleep(4000)
         work = threading.Thread(target=run_work)
         work.daemon = True
         work.start()
