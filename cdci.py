93fe3215bcec879e268f5d2d52eca11c899d9a5c
Shaobai Li <Shaobaili3@gmail.com>
2022-04-25 01:33:25 +1000

    add convert traditional chinese to chinese

--- cdci.py
+++ cdci.py
@@ -1,4 +1,12 @@
 # Update Riot set json files
 from decoder.api_wrapper import card
+import opencc
+
 card.downloadAllSet()
 card.downloadRawSet()
+
+with open('Resource/zh_tw.json', 'r', encoding='utf-8') as fp:
+    content = fp.read()
+    newContent = opencc.OpenCC('t2s').convert(content)
+    with open('Resource/zh_cn.json', 'w', encoding='utf-8') as output:
+        output.write(newContent)
