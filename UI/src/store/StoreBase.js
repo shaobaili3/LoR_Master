470674ab5bfe11b3cf03e814eff9f1b673aac582
painttist <painttist@gmail.com>
2022-04-28 12:19:42 +0800

    Change server to azure

--- UI/src/store/StoreBase.js
+++ UI/src/store/StoreBase.js
@@ -5,8 +5,8 @@ import sets_en from "../../../Resource/en_us.json"
 const sets_en_combined = [].concat(...sets_en)
 const API_WEB_BASE =
   process.env.VUE_APP_LMT_SERVER == "test"
-    ? "https://lormaster.herokuapp.com"
-    : "https://lormaster.herokuapp.com"
+    ? "https://lormaster.azurewebsites.net"
+    : "https://lormaster.azurewebsites.net"
 
 //https://lormaster.herokuapp.com
 //https://lormaster.azurewebsites.net
