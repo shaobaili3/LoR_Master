3f9867c07e117c2a2a36208ca44ac8480b159264
Shaobai Li <Shaobaili3@gmail.com>
2023-04-09 10:25:39 +0000

    fix riot data sets

--- decoder/api_wrapper/utils.py
+++ decoder/api_wrapper/utils.py
@@ -13,7 +13,7 @@ def read_json_file(json_file):
 
 def get_card_set_online(set_num: int, region="en_us"):
     url = f'http://dd.b.pvp.net/latest/set{set_num}/{region}/data/set{set_num}-{region}.json'
-    if set_num == 7:
+    if set_num == 8:
         url = f'http://dd.b.pvp.net/latest/set6cde/{region}/data/set6cde-{region}.json'
     print('Loading: ', url) 
     r = session.get(url)
